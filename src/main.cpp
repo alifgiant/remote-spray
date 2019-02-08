6eb9a943a757416a9e93064f38ca29666f308362
alifgiant <muh.alifgiant@gmail.com>
2019-02-09 00:42:45 +0700

    complete

---
+++ src/main.cpp
@@ -0,0 +1,115 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>  //https://github.com/esp8266/Arduino
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "shondaime";
+const char* password = "244466666";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char* spray_topic = "remote-spray-alif";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200); // start boudrate at 115200 Hz
+  Serial.println("--------------------------");
+  Serial.println("Remote Spray - Muhammad Alif Akbar");
+  Serial.println("--------------------------");
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    doSpray();
+  }
+}
+
+bool isSpraying = false;
+void doSpray() {
+  if (!isSpraying) {
+    isSpraying = true;
+    digitalWrite(12, HIGH);
+    digitalWrite(13, LOW);
+    delay(200);
+    digitalWrite(12, LOW);
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+    isSpraying = false;
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // subscribe to spray
+      client.subscribe(spray_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
